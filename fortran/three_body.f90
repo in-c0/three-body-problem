c11bad4d2b1c5d228cff96eaf55da109a913ac80
in-c0 <wldud5192@gmail.com>
2024-08-27 20:14:31 +1000

    Fortran stub

---
+++ fortran/three_body.f90
@@ -0,0 +1,6 @@
+module three_body
+contains
+    subroutine compute()
+        print *, "Running Fortran computation"
+    end subroutine compute
+end module three_body
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vulkan/vulkan.h>
+
+extern "C" void compute();  // Declaration of Fortran function
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello from C++!" << std::endl;
+    compute();  // Call Fortran subroutine
     return 0;
 }
