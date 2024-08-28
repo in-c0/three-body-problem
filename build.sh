2195b8e1f4d2cea2353246b994ec11eaf4545403
in-c0 <wldud5192@gmail.com>
2024-08-28 16:09:52 +1000

    Build automation included

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the build directory
+cd build
+
+# Remove all files in the build directory
+rm -rf *
+
+# Navigate back to the root directory
+cd ..
+
+# Run CMake to configure the build system
+cmake -B build
