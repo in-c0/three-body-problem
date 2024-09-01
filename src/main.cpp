e097a154ac5dc7886b37485cb4cb8c8542c4548b
in-c0 <wldud5192@gmail.com>
2024-09-01 18:41:09 +1000

    Finalising graphics pipeline setup

--- src/main.cpp
+++ src/main.cpp
@@ -257,6 +257,32 @@ private:
             throw std::runtime_error("failed to create pipeline layout!");
         }
 
+        // Now we combine all of the structures and objects from above, involving:
+            // Shader stages: the shader modules that define the functionality of the programmable stages of the graphics pipeline
+            // Fixed-function state: all of the structures that define the fixed-function stages of the pipeline, like input assembly, rasterizer, viewport and color blending
+            // Pipeline layout: the uniform and push values referenced by the shader that can be updated at draw time
+            // Render pass: the attachments referenced by the pipeline stages and their usage
+
+        VkGraphicsPipelineCreateInfo pipelineInfo{};
+        pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo.stageCount = 2;
+        pipelineInfo.pStages = shaderStages;
+        pipelineInfo.pVertexInputState = &vertexInputInfo;
+        pipelineInfo.pInputAssemblyState = &inputAssembly;
+        pipelineInfo.pViewportState = &viewportState;
+        pipelineInfo.pRasterizationState = &rasterizer;
+        pipelineInfo.pMultisampleState = &multisampling;
+        pipelineInfo.pColorBlendState = &colorBlending;
+        pipelineInfo.pDynamicState = &dynamicState;
+        pipelineInfo.layout = pipelineLayout;
+        pipelineInfo.renderPass = renderPass;
+        pipelineInfo.subpass = 0;
+        pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+        if (vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create graphics pipeline!");
+        }
+
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
     }
